20762f780433a0aa07d754c9a14eeaf42512dda5
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-11-01 16:11:25 +0530

    Created my first component that renders name dynamically

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+
+// Fn naming convention - Pascal Casing - first letter capitalised - stndard way of writing React apps
+function Message() {
+    // JSX - Javascript XML
+    const name = "";
+    if(name)
+        return <h1>Hello {name}</h1>
+    else
+    return <h1>Hello World</h1>
+}
+
+export default Message;
